1c7c1e60038fa582b620944246985ef050b79359
Jon Aldekoa <jaldekoa@gmail.com>
2024-09-19 20:45:58 +0200

    Fix test

--- nyfedapi/tests/test_pd.py
+++ nyfedapi/tests/test_pd.py
@@ -1,34 +1,34 @@
-import pandas as pd
-import nyfedapi
+from nyfedapi.pd import latest, get_all_timeseries, list_timeseries, list_asof, list_seriesbreaks, get_asof, get_timeseries, get_seriesbreaks_timeseries
+import pandas
 
 
 def test_pd_latest():
-    assert isinstance(nyfedapi.pd.latest(seriesbreak="SBN2022"), pd.DataFrame)
+    assert isinstance(latest(seriesbreak="SBN2022"), pandas.DataFrame)
 
 
 def test_pd_get_all_timeseries():
-    assert isinstance(nyfedapi.pd.get_all_timeseries(), pd.DataFrame)
+    assert isinstance(get_all_timeseries(), pandas.DataFrame)
 
 
 def test_pd_list_timeseries():
-    assert isinstance(nyfedapi.pd.list_timeseries(), pd.DataFrame)
+    assert isinstance(list_timeseries(), pandas.DataFrame)
 
 
 def test_pd_list_asof():
-    assert isinstance(nyfedapi.pd.list_asof(), pd.DataFrame)
+    assert isinstance(list_asof(), pandas.DataFrame)
 
 
 def test_pd_list_seriesbreaks():
-    assert isinstance(nyfedapi.pd.list_seriesbreaks(), pd.DataFrame)
+    assert isinstance(list_seriesbreaks(), pandas.DataFrame)
 
 
 def test_pd_get_asof():
-    assert isinstance(nyfedapi.pd.get_asof(date="2023-01-01"), pd.DataFrame)
+    assert isinstance(get_asof(date="2023-01-01"), pandas.DataFrame)
 
 
 def test_pd_get_timeseries():
-    assert isinstance(nyfedapi.pd.get_timeseries(timeseries="PDPOSGS-B_PDPOSGSC-L2"), pd.DataFrame)
+    assert isinstance(get_timeseries(timeseries="PDPOSGS-B_PDPOSGSC-L2"), pandas.DataFrame)
 
 
 def test_pd_get_seriesbreaks_timeseries():
-    assert isinstance(nyfedapi.pd.get_seriesbreaks_timeseries(seriesbreak="SBN2022", timeseries="PDPOSGSC-G11L21_PDPOSGSC-G2"), pd.DataFrame)
+    assert isinstance(get_seriesbreaks_timeseries(seriesbreak="SBN2022", timeseries="PDPOSGSC-G11L21_PDPOSGSC-G2"), pandas.DataFrame)
--- nyfedapi/tests/test_seclending.py
+++ nyfedapi/tests/test_seclending.py
@@ -1,5 +1,6 @@
 from nyfedapi import seclending
 import pandas as pd
+import pytest
 
 def test_seclending_results_latest():
     assert isinstance(seclending.results_latest(operation="all", include="summary"), pd.DataFrame)
@@ -17,5 +18,5 @@ def test_seclending_results_last_number():
 
 
 def test_seclending_results_search():
-    assert isinstance(seclending.results_search(operation="all", include="summary"), pd.DataFrame)
-    assert isinstance(seclending.results_search(operation="all", include="details"), pd.DataFrame)
+    assert isinstance(seclending.results_search(operation="all", include="summary", startDate="2021-01-01", endDate="2021-12-31"), pd.DataFrame)
+    assert isinstance(seclending.results_search(operation="all", include="details", startDate="2021-01-01", endDate="2021-12-31"), pd.DataFrame)
